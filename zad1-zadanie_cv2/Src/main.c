4c716193afed9f39a5bd4e7724d8d2edd5ef2664
xlulak <xlulak@stuba.sk>
2020-10-07 22:24:58 +0200

    uz to celkom slape

--- zad1-zadanie_cv2/Src/main.c
+++ zad1-zadanie_cv2/Src/main.c
@@ -86,7 +86,7 @@ int main(void)
 
 		    	  uint8_t pomocna =LED_STATUS;
 
-		    	  if(edge_type == FALL)
+		    	  if(edge_type == RISE)
 		    	  {
 		    		  if (LED_STATUS == 16) // 2 na 4tu
 		    		  {
